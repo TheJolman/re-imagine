85385b1035ca9ed042a8e89a89a7c7860641a719
josh <joshuaholman5@gmail.com>
2025-02-10 19:44:40 -0800

    formatting

--- src/main.c
+++ src/main.c
@@ -12,22 +12,61 @@
  ********************************************************************************************/
 
 #include "raylib.h"
+#include "raymath.h"
 
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(screenWidth, screenHeight, "Game!");
+
+  Vector2 playerPosition = {(float)screenWidth / 2, (float)screenHeight / 2};
+  float baseMoveSpeed = 2.0f;
 
   SetTargetFPS(60);
 
+  // Main game loop
   while (!WindowShouldClose()) {
+    // Update
+    // -------------------------------------------------------------------------
+    float moveSpeedModifier = 1.0f;
+    if (IsKeyDown(KEY_LEFT_SHIFT)) {
+      moveSpeedModifier = 2.0f;
+    }
+
+    float moveSpeed = baseMoveSpeed * moveSpeedModifier;
+    Vector2 moveDirection = {0.0f, 0.0f};
+
+    if (IsKeyDown(KEY_W)) {
+      moveDirection.y -= 1.0f;
+    }
+    if (IsKeyDown(KEY_S)) {
+      moveDirection.y += 1.0f;
+    }
+    if (IsKeyDown(KEY_A)) {
+      moveDirection.x -= 1.0f;
+    }
+    if (IsKeyDown(KEY_D)) {
+      moveDirection.x += 1.0f;
+    }
+
+    if (moveDirection.x != 0.0f || moveDirection.y != 0.0f) {
+      moveDirection = Vector2Normalize(moveDirection);
+      playerPosition.x += moveDirection.x * moveSpeed;
+      playerPosition.y += moveDirection.y * moveSpeed;
+    }
+    // -------------------------------------------------------------------------
+
+    // Draw
+    // -------------------------------------------------------------------------
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-    DrawText("Hi mom", 190, 200, 20, RED);
+
+    DrawCircleV(playerPosition, 20, RED);
 
     EndDrawing();
+    // -------------------------------------------------------------------------
   }
 
   CloseWindow();
