eed37d173274d4a7b5ae24350c706279e00b6c80
josh <joshuaholman5@gmail.com>
2025-02-24 16:44:39 -0800

    modularized

---
+++ src/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * Clamps a float between a min and max range
+ * @return float Original value if within range, or min/max otherwise
+ */
+float ClampFloat(float value, float min, float max);
+
+#endif
