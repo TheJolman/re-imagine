02437039d1b27a0fcc7ac5f08bef8be9731c0720
josh <joshuaholman5@gmail.com>
2025-08-07 19:26:06 -0700

    formatting

--- src/battle.c
+++ src/battle.c
@@ -175,8 +175,7 @@ static void actionMenuDisplay()
             {
                 actionMenu->items[index]->select();
                 actionMenuEnd();
-                            return;
-                
+                return;
             }
         }
     }
