39fd90c0ca043c111f76446583e9bd61473de51e
Josh Holman <josh@jolman.me>
2025-09-04 19:01:25 -0700

    refactor to use config struct

--- src/battle.c
+++ src/battle.c
@@ -11,12 +11,14 @@
 #include <assert.h>
 #include <stdint.h>
 
-// TODO: Move these somewhere better?
-static constexpr uint32_t WINDOW_MARGIN = 50;
-static constexpr uint32_t TEXT_HEIGHT = 150;
-static constexpr Color MON_TINT = WHITE;
-static constexpr float MON_ROTATION = 0.0;
-static constexpr float MON_SCALE = 0.6f;
+typedef struct
+{
+    uint32_t window_margin;
+    uint32_t text_height;
+    Color mon_tint;
+    float mon_rotation;
+    float mon_scale;
+} BattleUIConfig;
 
 typedef enum
 {
@@ -27,20 +29,32 @@ typedef enum
     BATTLE_SWITCH,
 } BattleState;
 
-static BattleState battle_state = BATTLE_MENU;
+typedef struct
+{
+    Mon *player_mon;
+    Mon *enemy_mon;
+    BattleUI *battle_ui;
+    bool initialized;
+    Menu *action_menu;
+    BattleState state;
+} BattleContext;
+
+static const BattleUIConfig ui_config = {
+    .window_margin = 50,
+    .text_height = 150,
+    .mon_tint = WHITE,
+    .mon_rotation = 0.0,
+    .mon_scale = 0.6f,
+};
+
+static BattleContext battle_ctx = {0};
 
 // clang-format off
-static void attack_select() { battle_state = BATTLE_ATTACK; }
-static void items_select()  { battle_state = BATTLE_ITEMS; }
-static void run_select()    { battle_state = BATTLE_RUN; }
-static void switch_select() { battle_state = BATTLE_SWITCH; }
+static void attack_select() { battle_ctx.state = BATTLE_ATTACK; }
+static void items_select()  { battle_ctx.state = BATTLE_ITEMS; }
+static void run_select()    { battle_ctx.state = BATTLE_RUN; }
+static void switch_select() { battle_ctx.state = BATTLE_SWITCH; }
 // clang-format on
-
-Mon *player_mon = nullptr;
-Mon *enemy_mon = nullptr;
-static BattleUI *battle_ui = nullptr;
-static bool battle_initialized = false;
-static Menu *action_menu = nullptr;
 
 static void action_menu_create()
 {
@@ -48,8 +62,9 @@ static void action_menu_create()
     void (*select_callbacks[])(void) = {attack_select, items_select, run_select, switch_select};
     MenuConfig config = {
         .title = "BATTLE MENU",
-        .rect = {battle_ui->text_box.x + battle_ui->text_box.width * 0.5f + 10,
-                 battle_ui->text_box.y + 30, 0, 0},
+        .rect = {battle_ctx.battle_ui->text_box.x + battle_ctx.battle_ui->text_box.width * 0.5f +
+                     10,
+                 battle_ctx.battle_ui->text_box.y + 30, 0, 0},
         .font_size = 20,
         .layout = MENU_LAYOUT_GRID,
         .num_rows = 2,
@@ -62,71 +77,75 @@ static void action_menu_create()
         error_log(res.err);
         return;
     }
-    action_menu = (Menu *)res.value;
+    battle_ctx.action_menu = (Menu *)res.value;
 }
 
 static void action_menu_end()
 {
-    if (action_menu)
-        menu_destroy(action_menu);
+    if (battle_ctx.action_menu)
+        menu_destroy(battle_ctx.action_menu);
 
-    action_menu = nullptr;
+    battle_ctx.action_menu = nullptr;
 }
 
 static void action_menu_display()
 {
-    if (!action_menu)
+    if (!battle_ctx.action_menu)
         action_menu_create();
 
     // Vertical line between text box and action menu
-    DrawLine(battle_ui->text_box.x + battle_ui->text_box.width * 0.5f, battle_ui->text_box.y,
-             battle_ui->text_box.x + battle_ui->text_box.width * 0.5f,
-             battle_ui->text_box.y + battle_ui->text_box.height, GRAY);
+    DrawLine(battle_ctx.battle_ui->text_box.x + battle_ctx.battle_ui->text_box.width * 0.5f,
+             battle_ctx.battle_ui->text_box.y,
+             battle_ctx.battle_ui->text_box.x + battle_ctx.battle_ui->text_box.width * 0.5f,
+             battle_ctx.battle_ui->text_box.y + battle_ctx.battle_ui->text_box.height, GRAY);
 
-    menu_draw(action_menu);
-    menu_handle_input(action_menu);
+    menu_draw(battle_ctx.action_menu);
+    menu_handle_input(battle_ctx.action_menu);
 }
 
 static void init_battle_ui(void)
 {
-    battle_ui = heap_list.malloc(sizeof(BattleUI));
-    if (!battle_ui)
+    battle_ctx.battle_ui = heap_list.malloc(sizeof(BattleUI));
+    if (!battle_ctx.battle_ui)
     {
         error_exit(1, "Could not allocate memory for BattleUI");
     }
 
-    battle_ui->text_box = (Rectangle){WINDOW_MARGIN, screen.height - (WINDOW_MARGIN + TEXT_HEIGHT),
-                                      screen.width - WINDOW_MARGIN * 2, TEXT_HEIGHT};
+    battle_ctx.battle_ui->text_box = (Rectangle){
+        ui_config.window_margin, screen.height - (ui_config.window_margin + ui_config.text_height),
+        screen.width - ui_config.window_margin * 2, ui_config.text_height};
 
     // TODO: Get consistent asset sizes
-    battle_ui->player_mon_pos = (Vector2){screen.width * 0.6f, screen.height * 0.35f};
-    battle_ui->enemy_mon_pos = (Vector2){screen.width * 0.05f, screen.height * 0.1f};
-    battle_ui->action_menu_pos = (Vector2){battle_ui->text_box.x + 20, battle_ui->text_box.y + 20};
-    battle_ui->status_bar_pos = (Vector2){battle_ui->text_box.x + 20, battle_ui->text_box.y + 80};
+    battle_ctx.battle_ui->player_mon_pos = (Vector2){screen.width * 0.6f, screen.height * 0.35f};
+    battle_ctx.battle_ui->enemy_mon_pos = (Vector2){screen.width * 0.05f, screen.height * 0.1f};
+    battle_ctx.battle_ui->action_menu_pos =
+        (Vector2){battle_ctx.battle_ui->text_box.x + 20, battle_ctx.battle_ui->text_box.y + 20};
+    battle_ctx.battle_ui->status_bar_pos =
+        (Vector2){battle_ctx.battle_ui->text_box.x + 20, battle_ctx.battle_ui->text_box.y + 80};
 
-    battle_state = BATTLE_MENU;
+    battle_ctx.state = BATTLE_MENU;
 
     // Initialize monsters (here froge is hardcoded in)
-    if (!player_mon)
+    if (!battle_ctx.player_mon)
     {
         Result res = create_mon("froge");
         if (res.err)
             error_exit(1, "%s", res.err);
 
-        player_mon = (Mon *)res.value;
-        player_mon->hp = 100;
-        load_mon_texture(player_mon, BACK);
+        battle_ctx.player_mon = (Mon *)res.value;
+        battle_ctx.player_mon->hp = 100;
+        load_mon_texture(battle_ctx.player_mon, BACK);
     }
 
-    if (!enemy_mon)
+    if (!battle_ctx.enemy_mon)
     {
         Result res = create_mon("froge");
         if (res.err)
             error_exit(1, "%s", res.err);
 
-        enemy_mon = (Mon *)res.value;
-        enemy_mon->hp = 80;
-        load_mon_texture(enemy_mon, FRONT);
+        battle_ctx.enemy_mon = (Mon *)res.value;
+        battle_ctx.enemy_mon->hp = 80;
+        load_mon_texture(battle_ctx.enemy_mon, FRONT);
     }
 }
 
@@ -135,18 +154,20 @@ static void render_mon(Mon *mon, Vector2 position)
     if (!mon || !mon->texture || !IsTextureValid(*mon->texture))
         return;
 
-    DrawTextureEx(*mon->texture, position, MON_ROTATION, MON_SCALE, MON_TINT);
+    DrawTextureEx(*mon->texture, position, ui_config.mon_rotation, ui_config.mon_scale,
+                  ui_config.mon_tint);
 }
 
 static void render_text_box(void)
 {
-    DrawRectangleLines(battle_ui->text_box.x, battle_ui->text_box.y, battle_ui->text_box.width,
-                       battle_ui->text_box.height, WHITE);
+    DrawRectangleLines(battle_ctx.battle_ui->text_box.x, battle_ctx.battle_ui->text_box.y,
+                       battle_ctx.battle_ui->text_box.width, battle_ctx.battle_ui->text_box.height,
+                       WHITE);
 }
 
 static void render_action_menu(void)
 {
-    switch (battle_state)
+    switch (battle_ctx.state)
     {
     case BATTLE_MENU:
         action_menu_display();
@@ -171,19 +192,19 @@ static void render_battle_ui(void)
     render_text_box();
     render_action_menu();
 
-    if (player_mon)
-        render_mon(player_mon, battle_ui->player_mon_pos);
+    if (battle_ctx.player_mon)
+        render_mon(battle_ctx.player_mon, battle_ctx.battle_ui->player_mon_pos);
 
-    if (enemy_mon)
-        render_mon(enemy_mon, battle_ui->enemy_mon_pos);
+    if (battle_ctx.enemy_mon)
+        render_mon(battle_ctx.enemy_mon, battle_ctx.battle_ui->enemy_mon_pos);
 }
 
 void battle_scene_render(void)
 {
-    if (!battle_initialized)
+    if (!battle_ctx.initialized)
     {
         init_battle_ui();
-        battle_initialized = true;
+        battle_ctx.initialized = true;
     }
 
     DrawText("Battle scene is active!\nPress B to go back!", 50, 50, 20, DARKGRAY);
@@ -192,25 +213,25 @@ void battle_scene_render(void)
 
 void battle_scene_end(void)
 {
-    if (player_mon)
+    if (battle_ctx.player_mon)
     {
-        destroy_mon(player_mon);
-        player_mon = nullptr;
+        destroy_mon(battle_ctx.player_mon);
+        battle_ctx.player_mon = nullptr;
     }
 
-    if (enemy_mon)
+    if (battle_ctx.enemy_mon)
     {
-        destroy_mon(enemy_mon);
-        enemy_mon = nullptr;
+        destroy_mon(battle_ctx.enemy_mon);
+        battle_ctx.enemy_mon = nullptr;
     }
 
-    if (battle_ui)
+    if (battle_ctx.battle_ui)
     {
-        heap_list.free(battle_ui);
-        battle_ui = nullptr;
+        heap_list.free(battle_ctx.battle_ui);
+        battle_ctx.battle_ui = nullptr;
     }
 
     action_menu_end();
-    battle_state = BATTLE_MENU;
-    battle_initialized = false;
+    battle_ctx.state = BATTLE_MENU;
+    battle_ctx.initialized = false;
 }
