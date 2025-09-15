f841fa1439a46c1c8eb0e497eda2fe34f2ef02b1
Josh Holman <josh@jolman.me>
2025-09-15 12:16:01 -0700

    main.c qol changes

--- src/main.c
+++ src/main.c
@@ -29,15 +29,21 @@
 #define GIT_VERSION "unknown"
 #endif
 
-constexpr size_t SCREEN_WIDTH_INITIAL = 1200;
-constexpr size_t SCREEN_HEIGHT_INITIAL = 900;
+static constexpr struct
+{
+    size_t screen_width_initial;
+    size_t screen_height_initial;
+    size_t screen_width_min;
+    size_t screen_height_min;
+    size_t fps_target;
+} cfg = {1200, 900, 400, 300, 60};
 
 /**
  * @returns true if target matches short_arg or long_arg.
  * @returns false otherwise
  * If only short_arg or long_arg is needed, pass nullptr in place of the other.
  */
-bool match_arg(const char *target, const char *short_arg, const char *long_arg)
+static bool _match_arg(const char *target, const char *short_arg, const char *long_arg)
 {
     return (short_arg && strcmp(target, short_arg) == 0 ||
             long_arg && strcmp(target, long_arg) == 0);
@@ -48,12 +54,12 @@ int main(int argc, const char **argv)
     // ----------- Command line argument parsing -----------
     for (int i = 1; i < argc; i++)
     {
-        if (match_arg(argv[i], "-v", "--version"))
+        if (_match_arg(argv[i], "-v", "--version"))
         {
             printf("%s %s\n", argv[0], GIT_VERSION);
             exit(0);
         }
-        else if (match_arg(argv[i], "-h", "--help"))
+        else if (_match_arg(argv[i], "-h", "--help"))
         {
             printf("Usage: %s [-h | --help] [-v | --version]\n"
                    "Have fun!\n",
@@ -74,23 +80,22 @@ int main(int argc, const char **argv)
     heap_list = heap_list_create();
     atexit(free_all);
 
-    debug_log("Game initiated with screen dimensions %dx%d", SCREEN_WIDTH_INITIAL,
-              SCREEN_HEIGHT_INITIAL);
-
     SetWindowState(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
-    InitWindow(SCREEN_WIDTH_INITIAL, SCREEN_HEIGHT_INITIAL, "Game!");
-    SetWindowMinSize(400, 300);
+    InitWindow(cfg.screen_width_initial, cfg.screen_height_initial, "Game!");
+    SetWindowMinSize(cfg.screen_width_min, cfg.screen_height_min);
 
     if (!IsWindowReady())
     {
         error_exit(1, "failed to initialize window");
     }
 
-    SetTargetFPS(60);
+    debug_log("Game initiated with screen dimensions %dx%d", cfg.screen_width_initial,
+              cfg.screen_height_initial);
+
+    SetTargetFPS(cfg.fps_target);
     SetExitKey(KEY_NULL);
 
     game_init();
-
 
     // Main game loop
     while (!WindowShouldClose())
